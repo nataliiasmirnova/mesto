899068f8cade468d5f19bd9fe62e1e71a91e3f52
Nataliia Smirnova <ensueno11@gmail.com>
2022-01-23 18:00:29 +0200

    Попап/открытие-закрытие

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("hello");
+const profileEditButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
+const popupCloseButton = document.querySelector('.popup__close-button')
+const popupContainer = document.querySelector('.popup__container')
+
+function openPopup(event) {
+    popup.classList.add('popup_opened')
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened')
+}
+
+function backgroundClick(event) {
+    if (event.target === event.currentTarget) {
+        closePopup()
+    }
+}
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popup.addEventListener('click', backgroundClick);
