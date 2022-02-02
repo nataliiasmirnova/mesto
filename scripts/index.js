744afaa01abc59760ab1f824c7c4642a4bb6b5cf
Nataliia Smirnova <ensueno11@gmail.com>
2022-02-02 20:36:20 +0200

    Правки css/blocks

--- scripts/index.js
+++ scripts/index.js
@@ -10,39 +10,40 @@ const professionInput = document.getElementById('field-profession');
 const nameOutput = document.querySelector('.profile__name');
 const professionOutput = document.querySelector('.profile__profession');
 
+//открытие попап//
 function openPopup(event) {
-    event.preventDefault()
+    /*event.preventDefault()*/
     popup.classList.add('popup_opened')
 }
 
-
+//закрытие попап через кнопку закрытия//
 function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
+/*//закрытие поап через клик вне зоны поап//
 function backgroundClick(event) {
     if (event.target === event.currentTarget) {
         closePopup()
     }
-}
+}*/
 
+//автозаполнение поапа данными из профиля//
 function openEditPopup(event) {
     openPopup(event);
     nameInput.value = nameOutput.textContent;
     professionInput.value = professionOutput.textContent;
 }
 
-profileEditButton.addEventListener('click', openEditPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popup.addEventListener('click', backgroundClick);
-popupContainer.addEventListener('submit', formSubmitHandler);
-
-
-//редактирование инфо в профиле//
-
+//редактирование и сохранение инфо в профиле//
 function formSubmitHandler(evt) {
     evt.preventDefault();
     nameOutput.textContent = nameInput.value;
     professionOutput.textContent = professionInput.value;
     closePopup()
 }
+
+profileEditButton.addEventListener('click', openEditPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupContainer.addEventListener('submit', formSubmitHandler);
+/*popup.addEventListener('click', backgroundClick);*/
