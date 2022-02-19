5abfbbf8b72ee2044a5946a191cd77002c9a2619
Nataliia Smirnova <ensueno11@gmail.com>
2022-02-19 20:29:14 +0200

    hotfix/photo-grid_image_links

--- scripts/gridCards.js
+++ scripts/gridCards.js
@@ -1,26 +1,26 @@
 const gridCards = [{
         name: "Гауя",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-gauja.jpg",
+        link: "./images/photo-grid-gauja.jpg",
     },
 
     {
         name: "Балтика",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-baltic-sea.jpg",
+        link: "./images/photo-grid-baltic-sea.jpg",
     },
     {
         name: "Кулдига",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-kuldiga.jpg",
+        link: "./images/photo-grid-kuldiga.jpg",
     },
     {
         name: "Терветский национальный парк",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-tervete.jpg",
+        link: "./images/photo-grid-tervete.jpg",
     },
     {
         name: "Старая Рига",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-riga.jpg",
+        link: "./images/photo-grid-riga.jpg",
     },
     {
         name: "Огре",
-        link: "file:///Users/natalie/Desktop/project_4/mesto/images/photo-grid-ogre.jpg",
+        link: "./images/photo-grid-ogre.jpg",
     },
 ];
